dd99fa9f848f1a3186f02caf17aac640a7d530eb
lzc7 <1065469529@qq.com>
2018-07-12 10:47:41 +0800

    删除了target

--- target/classes/com/zipi/service/RmbboxLoanServiceRepaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.zipi.service;
-
-import java.math.BigDecimal;
-
-public interface RmbboxLoanServiceRepaymentService {
-
-    BigDecimal getLoanServiceAmountByRepaymentId(String repayment);
-
-}
